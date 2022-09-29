c04f277deb62189620d89c1ab1a6fec8bb58ad8a
Ricardo Vieira <ricardovieira44@gmail.com>
2022-09-29 16:36:45 +0100

    Commit after point 5.

---
+++ src/main/java/Application.java
@@ -0,0 +1,34 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new
+                DefaultTerminalFactory()
+                .setInitialTerminalSize(terminalSize);
+        Screen screen;
+        screen = null;
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert screen != null;
+        screen.clear();
+        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
+                [0]);
+        screen.refresh();
+    }
+}
