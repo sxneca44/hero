f367c786f374da40d06ebe1e1340a96da23c3e2c
Ricardo Vieira <ricardovieira44@gmail.com>
2022-10-06 16:29:39 +0100

    Commit after point 12.

--- src/main/java/com/sxneca44/hero/Arena.java
+++ src/main/java/com/sxneca44/hero/Arena.java
@@ -8,18 +8,33 @@ import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
     private int width;
     private int height;
     private Hero hero;
+    private List<Wall> walls;
 
     public Arena(int width, int height) {
+        this.walls = createWalls();
         hero = new Hero(10, 10);
         this.width = width;
         this.height = height;
     }
-
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+        return walls;
+    }
     public void draw(TextGraphics graphics) {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
         graphics.fillRectangle(new TerminalPosition(0,0), new TerminalSize(width, height), ' ');
--- src/main/java/com/sxneca44/hero/Wall.java
+++ src/main/java/com/sxneca44/hero/Wall.java
@@ -5,35 +5,34 @@ import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
-public class Hero {
+public class Wall {
+
     public Position getPosition() {
         return this.position;
     }
 
-    private Position position;
-
-    public Hero(int x, int y)
+    public Wall(int x, int y)
     {
         this.position = new Position(x, y);
     }
+    private Position position;
+
     public void setPosition(Position position){
         this.position = position;
     }
-    public Position moveUp(){
-        return new Position(position.getX(), position.getY()-1);
-    }
-    public Position moveDown(){
-        return new Position(position.getX(), position.getY()+1);
-    }
-    public Position moveLeft(){
-        return new Position(position.getX()-1, position.getY());
-    }
-    public Position moveRight(){
-        return new Position(position.getX()+1, position.getY());
-    }
     public void draw(TextGraphics graphics) {
-        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
+        graphics.setForegroundColor(TextColor.Factory.fromString("#D21FB2"));
         graphics.enableModifiers(SGR.BOLD);
         graphics.putString(new TerminalPosition(position.getX(), position.getY()), "X");
     }
+
+
+
+
+
+
+
+
+
+
 }
